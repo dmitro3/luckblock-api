8fcf14cd708b4b0d4e4a1f705313b1422bb20529
Androz2091 <androz2091@gmail.com>
2023-06-23 13:54:43 +0200

    fix db

--- postgres.js
+++ postgres.js
@@ -2,7 +2,8 @@ const { Sequelize, DataTypes } = require('sequelize');
 
 const sequelize = new Sequelize(process.env.PG_DATABASE, process.env.PG_USER, process.env.PG_PASSWORD, {
 	host: process.env.PG_HOST,
-	dialect: 'postgres'
+	dialect: 'postgres',
+	sync: { alter: true }
 });
 
 const ContractAudit = sequelize.define('ContractAudit', {
